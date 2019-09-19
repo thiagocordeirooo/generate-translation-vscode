f60137e71e2bcd9899638764e90e6adc656bfdab
Captain Caius <241342+captaincaius@users.noreply.github.com>
2019-09-19 05:39:04 +0300

    add ability to use selected text as one of the languages' translation

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ A visual studio code extension for you to generate the translations without leav
 This extension contributes the following settings:
 
 - `generate-translation.path`: Path to find your i18n files.
+- `generate-translation.defaultLanguageFile`: Default language file (for extracting existing translation).
 - `generate-translation.sort`: Sort object after inserting translation.
 - `generate-translation.replaceOnTranslate`: Replace the selected text in HTML files after generating a translation string.
 - `generate-translation.templateHtmlToReplace`: Template HTML to replace the selected text after generating a translation string. \n The string i18n will be replaced by the chosen key.
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,15 @@
-import { ExtensionContext, commands, window } from 'vscode';
+import { ExtensionContext, commands, window, workspace } from 'vscode';
 import { GenerateTranslation } from './lib';
 
 export const activate = (context: ExtensionContext) => {
 	const generateTranslationGenerate = commands.registerCommand('generateTranslation.generate', generate);
 	context.subscriptions.push(generateTranslationGenerate);
 
-	const generateTranslationFromSelectedText = commands.registerCommand('generateTranslation.fromSelectedText', fromSelectedText);
-	context.subscriptions.push(generateTranslationFromSelectedText);
+	const generateTranslationFromSelectedTranslatedText = commands.registerCommand('generateTranslation.fromSelectedTranslatedText', fromSelectedTranslatedText);
+	context.subscriptions.push(generateTranslationFromSelectedTranslatedText);
+
+	const generateTranslationFromSelectedTranslationKey = commands.registerCommand('generateTranslation.fromSelectedTranslationKey', fromSelectedTranslationKey);
+	context.subscriptions.push(generateTranslationFromSelectedTranslationKey);
 };
 
 const generate = async () => {
@@ -19,7 +22,43 @@ const generate = async () => {
 	}
 };
 
-const fromSelectedText = () => {
+const fromSelectedTranslatedText = async () => {
+	const editor = window.activeTextEditor;
+	if (!editor) {
+		return;
+	}
+
+	const selection = editor.selection;
+	const textSelection = editor.document.getText(selection);
+	if (!textSelection) {
+		window.showWarningMessage('Nothing selected.');
+		return;
+  }
+
+  const defaultLanguageFile = <string>workspace
+    .getConfiguration("generate-translation")
+    .get("defaultLanguageFile") || "";
+  const selectionLanguage = await window.showInputBox({
+    prompt: `What is the language file corresponding to the current selection's language?`,
+    value: defaultLanguageFile
+  });
+	if (!selectionLanguage) {
+		window.showWarningMessage('You must choose a language corresponding to the selection.');
+		return;
+  }
+
+	const key = await window.showInputBox({
+		prompt: `Which key do you want to use?`,
+	});
+	if (!key) {
+		window.showWarningMessage('You must choose a key for the translation.');
+		return;
+  }
+
+	GenerateTranslation.fromSelectedTranslatedText(key, textSelection, selectionLanguage);
+};
+
+const fromSelectedTranslationKey = () => {
 	const editor = window.activeTextEditor;
 	if (!editor) {
 		return;
@@ -33,5 +72,5 @@ const fromSelectedText = () => {
 		return;
 	}
 
-	GenerateTranslation.fromSelectedText(textSelection);
+	GenerateTranslation.fromSelectedTranslationKey(textSelection);
 };
